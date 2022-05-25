6b52bc7de3102664a2891b05c69d6b4720b5fd72
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-05-25 16:06:38 +0530

    updates

--- go/videochat/internal/server/server.go
+++ go/videochat/internal/server/server.go
@@ -3,6 +3,14 @@ package server
 import (
 	"flag"
 	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+	"github.com/m3rashid/learn_x/go/videochat/internal/handlers"
 )
 
 var (
@@ -17,10 +25,18 @@ func Run() error {
 		*addr = ":5000"
 	}
 
+	engine := html.new("./views", ".html")
+	app := fiber.New(fiber.Config{ Views: engine })
+	app.Use(logger.New())
+	app.Use(cors.New())
+
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket")
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.roomwebsocket, websocket.Config{
+		HandshakeTimeout: 10*time.Second,
+	}))
+
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomChatWebsocket))
