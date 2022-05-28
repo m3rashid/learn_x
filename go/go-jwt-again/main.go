e493e3c479a181ff0e4bb94d15e44fefe4232eff
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-05-28 09:36:17 +0530

    jwt

---
+++ go/go-jwt-again/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var signingKey = []byte(os.Getenv("JWT_SEC"))
+
+func GenerateJwt() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"] = true
+	claims["user"] = "admin"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	tokenString, err := token.SignedString(signingKey) // this is the signing key
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err.Error())
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func main() {
+	fmt.Println("Simple client")
+
+	tokenString, err := GenerateJwt()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(tokenString)
+}
