e60dbb511cf0f3a205fc11a1579b8ef56fe8b2e4
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-07-02 15:45:54 +0530

    solid

---
+++ pretty-solid-js/repo-finder/src/App.tsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "solid-app-router";
+import { Component, createEffect, createSignal, lazy } from "solid-js";
+import Nav from "./components/nav";
+
+const Home = lazy(() => import("./pages/home"));
+const Saved = lazy(() => import("./pages/saved"));
+
+const [username, setUserName] = createSignal("m3rashid");
+const [repos, setRepos] = createSignal([]);
+
+createEffect(async () => {
+  const res = await fetch(`https://api.github.com/users/${username()}/repos`, {
+    headers: {
+      Authorization: "token <TOKEN>",
+      Accept: "application/vnd.github+json",
+    },
+  });
+  console.log(res);
+  setRepos(await res.json());
+});
+
+const App: Component = () => {
+  return (
+    <div class="container">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </div>
+  );
+};
+
+export { username, setUserName, repos };
+export default App;
---
+++ pretty-solid-js/repo-finder/src/pages/saved.tsx
@@ -0,0 +1,21 @@
+import { Component, createSignal, For } from "solid-js";
+import RepoCard, { Repo } from "../components/repoCard";
+
+const reposFromLocalStorage = JSON.parse(
+  localStorage.getItem("savedRepos") || "[]"
+);
+const [savedRepos, setSavedRepos] = createSignal(
+  reposFromLocalStorage as Repo[]
+);
+
+const Saved: Component = () => {
+  return (
+    <div>
+      <h2>Your saved repos</h2>
+      <For each={savedRepos()}>{(repo: Repo) => <RepoCard repo={repo} />}</For>
+    </div>
+  );
+};
+
+export { savedRepos, setSavedRepos };
+export default Saved;
